349b848ba20f3b2d31a8b91926cf9b9ecec41415
rebecca9201 <wangmmqq@gmail.com>
2017-04-16 20:52:46 +0800

    finish 4-3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,16 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:create, :destroy]
 
   def index
-    @posts = Post.order("id DESC").all    # 新贴文放前面
+    @posts = Post.order("id DESC").limit(20)
+
+    if params[:max_id]
+      @posts = @posts.where( "id < ?", params[:max_id])
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end   
   end
 
   def create
@@ -15,7 +24,7 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
     @post.destroy
-
+    render :json => { :id => @post.id }
   end
 
  def like
