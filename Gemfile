4e76ff093172d6eef49b4b6a79c1dfa8c7e9ff88
rebecca9201 <wangmmqq@gmail.com>
2017-09-11 19:34:52 +0800

    add capistrano

--- Gemfile
+++ Gemfile
@@ -33,10 +33,13 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem "devise"
+gem "mysql2"
 
 gem 'bootstrap-sass', '~> 3.3.6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'capistrano-rails'
+  gem 'capistrano-passenger'
   gem 'byebug', platform: :mri
 end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,23 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
     @post.destroy
+    render :json => { :id => @post.id }
   end
+
+  def like
+    @post = Post.find(params[:id])
+    unless @post.find_like(current_user)  # 如果已经按讚过了，就略过不再新增
+      Like.create( :user => current_user, :post => @post)
+    end
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+    render "like"
+  end
+
 
 
   protected
